9705e1c12a014282aff86fc343a3a24bbd038132
Atharva Rokade <143008057+atharvarokade17@users.noreply.github.com>
2025-02-12 00:24:08 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,6 +5,6 @@ set -e
 docker pull abhishek5/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishek5/simple-python-flask-app
+docker run -d -p 5000:8000 abhishek5/simple-python-flask-app
 
 
